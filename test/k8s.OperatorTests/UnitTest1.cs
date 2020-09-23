fdd3a817238a93db1b064f3fefc5e6ae7c8f54dc
Alberto Falossi <26248823+falox@users.noreply.github.com>
2020-09-23 18:28:56 +0200

    Added KubernetesClient package

---
+++ test/k8s.OperatorTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using k8s.Operator;
+
+namespace k8s.OperatorTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
